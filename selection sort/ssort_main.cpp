593b0f18986aeb3bad575709081c51dd2add7602
vint10tov <viktordesiatov@mail.ru>
2024-04-21 08:31:27 +0600

    commit message2

---
+++ selection sort/ssort_main.cpp
@@ -0,0 +1,39 @@
+#include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+template <typename T>
+void selection_sort(T * arr, int arr_size);
+
+const int SIZE = 50;
+
+int main() { 
+    int arrs[SIZE];
+    std::srand(time(0));
+    for (size_t i = 0; i < SIZE; ++i) {
+        arrs[i] = rand() % 10000;
+        std::cout << arrs[i] << std::endl;
+    }
+    std::cout << "Sorted array" << std::endl;
+    selection_sort(arrs, SIZE);
+    for (size_t i = 0; i < SIZE; ++i)
+        std::cout << arrs[i] << std::endl;
+    return 0;
+}
+
+template <typename T>
+void selection_sort(T * arr, int arr_size) {
+    for (size_t i = 0; i < arr_size - 1; ++i) {
+        size_t min_index = i;
+        for (size_t j = i + 1; j < arr_size; ++j) {
+            if (arr[j] < arr[min_index])
+                min_index = j;
+        }
+        if (min_index != i) {
+            T temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+    }
+}
