803f357c1a56fde53ae0a51c514c6252b794511d
vint10tov <viktordesiatov@mail.ru>
2024-04-21 13:58:42 +0600

    commit message3

--- selection sort/ssort_main.cpp
+++ selection sort/ssort_main.cpp
@@ -24,16 +24,18 @@ int main() {
 
 template <typename T>
 void selection_sort(T * arr, int arr_size) {
-    for (size_t i = 0; i < arr_size - 1; ++i) {
-        size_t min_index = i;
-        for (size_t j = i + 1; j < arr_size; ++j) {
-            if (arr[j] < arr[min_index])
-                min_index = j;
-        }
-        if (min_index != i) {
-            T temp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = temp;
+    if (arr_size != 0 and arr_size != 1) {
+        for (size_t i = 0; i < arr_size - 1; ++i) {
+            size_t min_index = i;
+            for (size_t j = i + 1; j < arr_size; ++j) {
+                if (arr[j] < arr[min_index])
+                    min_index = j;
+            }
+            if (min_index != i) {
+                T temp = arr[i];
+                arr[i] = arr[min_index];
+                arr[min_index] = temp;
+            }
         }
     }
 }
